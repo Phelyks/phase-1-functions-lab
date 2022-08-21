1508378a0cf5e4ce3c54751b4a1a288853f94aa0
Felix <flexxmosh@gmail.com>
2022-08-21 16:19:56 +0300

    Assignment Done

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Code your solution in this file!
+const hq = 42;
+function distanceFromHqInBlocks(distance){
+   if (distance <42 ){
+       return hq-distance
+   } else {
+       return distance - hq
+   }
+}
+
+function distanceFromHqInFeet(distance) {
+    return distanceFromHqInBlocks(distance) *264
+    if ( distance < 42)
+    returns (hq-distance)*264
+    else {
+        return (distance - hq)*264
+    }
+}
+
+function distanceTravelledInFeet(start, destination){
+    if(start < destination){
+      return (destination - start)*264
+    } else {
+      return (start - destination)*264
+    }
+  }
+
+  function calculatesFarePrice(start, destination){
+    let distance = distanceTravelledInFeet(start, destination)
+    if(distance <= 400){
+      return 0
+    } else if (distance > 401 && distance <= 2000){
+      return 2.56
+    } else if (distance > 2000 && distance < 2500) {
+      return 25
+    } else {
+      return 'cannot travel that far'
+    }
+  } // Code your solution in this file!
